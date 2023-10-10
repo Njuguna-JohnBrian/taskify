af38b669d6d7a42f1550a9382ac0c83120c6432a
John Brian <njugunajb96@gmail.com>
2023-10-10 21:46:03 +0300

    fix button styling

--- lib/common/widgets/custom_otn_btn.dart
+++ lib/common/widgets/custom_otn_btn.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:taskify/common/widgets/reusable_text.dart';
+
+import 'appstyle.dart';
+
 
 class CustomOutlnBtn extends StatelessWidget {
   const CustomOutlnBtn({
@@ -7,12 +11,16 @@ class CustomOutlnBtn extends StatelessWidget {
     this.color2,
     required this.width,
     required this.height,
+    required this.text,
+    required this.color,
   });
 
   final void Function()? onTap;
   final double width;
   final double height;
   final Color? color2;
+  final Color color;
+  final String text;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -22,8 +30,21 @@ class CustomOutlnBtn extends StatelessWidget {
         height: height,
         decoration: BoxDecoration(
           color: color2,
-          borderRadius:  BorderRadius.circular(20),
-          border: Border.all(width: 1, color: color2)
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            width: 1,
+            color: color,
+          ),
+        ),
+        child: Center(
+          child: ReusableText(
+            text: text,
+            style: appstyle(
+              18,
+              color,
+              FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
