332ac9e09afdf327d4421859488c5b3d47283057
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-14 14:53:51 +0300

    add logic to add todo

--- lib/features/todo/pages/add.dart
+++ lib/features/todo/pages/add.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
     as picker;
+import 'package:taskify/common/models/task_model.dart';
 
 import 'package:taskify/common/utils/constants.dart';
 import 'package:taskify/common/widgets/appstyle.dart';
@@ -10,6 +11,7 @@ import 'package:taskify/common/widgets/custom_otn_btn.dart';
 import 'package:taskify/common/widgets/custom_text.dart';
 import 'package:taskify/common/widgets/height_spacer.dart';
 import 'package:taskify/features/todo/controllers/dates/dates_provider.dart';
+import 'package:taskify/features/todo/controllers/todo_provider.dart';
 
 class AddTask extends ConsumerStatefulWidget {
   const AddTask({super.key});
@@ -25,6 +27,8 @@ class _AddTaskState extends ConsumerState<AddTask> {
   @override
   Widget build(BuildContext context) {
     var scheduleDate = ref.watch(dateStateProvider);
+    var scheduleStartTime = ref.watch(startTimeStateProvider);
+    var scheduleFinishTime = ref.watch(finishTimeStateProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -103,20 +107,60 @@ class _AddTaskState extends ConsumerState<AddTask> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 CustomOutlnBtn(
-                  onTap: () {},
+                  onTap: () {
+                    picker.DatePicker.showDateTimePicker(
+                      context,
+                      showTitleActions: true,
+                      minTime: DateTime(2023, 6, 01, 50),
+                      maxTime: DateTime(2024, 6, 7, 09),
+                      onConfirm: (startTime) {
+                        ref.read(startTimeStateProvider.notifier).setStartTime(
+                              startTime.toString(),
+                            );
+                      },
+                      locale: picker.LocaleType.en,
+                    );
+                  },
                   width: AppConsts.kWidth * 0.4,
                   height: 52.h,
                   color: AppConsts.kLight,
                   color2: AppConsts.kBlueLight,
-                  text: "Start Time",
+                  text: scheduleStartTime == ""
+                      ? "Start Time"
+                      : scheduleStartTime.substring(
+                          10,
+                          16,
+                        ),
                 ),
                 CustomOutlnBtn(
-                  onTap: () {},
+                  onTap: () {
+                    picker.DatePicker.showDateTimePicker(
+                      context,
+                      showTitleActions: true,
+                      minTime: DateTime(2023, 6, 01, 50),
+                      maxTime: DateTime(2024, 6, 7, 09),
+                      onConfirm: (finishTime) {
+                        ref
+                            .read(
+                              finishTimeStateProvider.notifier,
+                            )
+                            .setFinishTime(
+                              finishTime.toString(),
+                            );
+                      },
+                      locale: picker.LocaleType.en,
+                    );
+                  },
                   width: AppConsts.kWidth * 0.4,
                   height: 52.h,
                   color: AppConsts.kLight,
                   color2: AppConsts.kBlueLight,
-                  text: "End Time",
+                  text: scheduleFinishTime == ""
+                      ? "End Time"
+                      : scheduleFinishTime.substring(
+                          10,
+                          16,
+                        ),
                 ),
               ],
             ),
@@ -124,6 +168,38 @@ class _AddTaskState extends ConsumerState<AddTask> {
               hieght: 20,
             ),
             CustomOutlnBtn(
+              onTap: () {
+                if (title.text.isNotEmpty &&
+                    desc.text.isNotEmpty &&
+                    scheduleFinishTime.isNotEmpty &&
+                    scheduleDate.isNotEmpty &&
+                    scheduleStartTime.isNotEmpty) {
+                  TaskModel taskModel = TaskModel(
+                    title: title.text,
+                    desc: desc.text,
+                    isCompleted: 0,
+                    date: scheduleDate,
+                    startTime: scheduleStartTime.substring(
+                      10,
+                      16,
+                    ),
+                    endTime: scheduleFinishTime.substring(
+                      10,
+                      16,
+                    ),
+                    remind: 0,
+                    repeat: "yes",
+                  );
+
+                  ref.read(todoStateProvider.notifier).addTodo(taskModel);
+                  ref.read(finishTimeStateProvider.notifier).setFinishTime("");
+                  ref.read(startTimeStateProvider.notifier).setStartTime("");
+                  ref.read(dateStateProvider.notifier).setDate("");
+                  Navigator.pop(context);
+                } else {
+                  print("failed to add task");
+                }
+              },
               width: AppConsts.kWidth * 0.4,
               height: 52.h,
               color: AppConsts.kLight,
